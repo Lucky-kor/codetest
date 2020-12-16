b232f3eb8cfcb58487371287ccd459a7b2939f11
Lucky-kor <jungmin2056@gmail.com>
2020-12-17 00:00:25 +0900

    Create a method to store the same number in an array After removing the same number among saved values, sort in ascending order

--- src/arrayTwoPickSum.java
+++ src/arrayTwoPickSum.java
@@ -1,12 +1,38 @@
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.Scanner;
 
 public class arrayTwoPickSum {
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
         String scanWords = scan.nextLine();
+        ArrayList<Integer> arraySumList = new ArrayList<Integer>();
 
-        
+        for (int i = 0; i < arrayWords((scanWords)).length - 1; i++) {
+            for (int j = i + 1; j < arrayWords(scanWords).length; j++) {
+                arraySumList.add(arrayWords(scanWords)[i] + arrayWords(scanWords)[j]);
+            }
+        }
+
+        for (int i = 0; i < arraySumList.size() - 1; i++) {
+            for (int j = i + 1; j < arraySumList.size(); j++) {
+                if (arraySumList.get(i).equals(arraySumList.get(j))) {
+                    arraySumList.remove(j);
+                    j--;
+                } else {
+                    continue;
+                }
+            }
+        }
+        arraySumList.sort(Comparator.naturalOrder());
+
+        for (int i = 0; i < arraySumList.size(); i++) {
+            System.out.println(arraySumList.get(i));
+        }
+
     }
+
     private static int arraysSize(String scanWords) {   //배열의 크기 구하기
         int arraySizes = 0;
         for (int k = 0; k < scanWords.length(); k++) {
@@ -19,6 +45,7 @@ public class arrayTwoPickSum {
         }
         return arraySizes;
     }
+
     private static int[] arrayWords(String scanWords) {     //배열안의 숫자 구해넣기
         int[] words = new int[arraysSize(scanWords)];
         String checkWords = null;
