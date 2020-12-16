431f595643ed738f23fde05f0cfc01ced496071b
Lucky-kor <jungmin2056@gmail.com>
2020-12-17 04:07:00 +0900

    Improve processing efficiency refactoring codes

--- src/notFinishPlayer.java
+++ src/notFinishPlayer.java
@@ -7,59 +7,68 @@ public class notFinishPlayer {
         String participant = scan.nextLine();
         String completion = scan.nextLine();
 
+        System.out.println(solution(participant, completion));
+    }
+
+    private static ArrayList<String> getParticipant(String participant) {
+        int countFirst = 0;
+        ArrayList<String> name = new ArrayList<String>();
+
+        for (int i = 0; i < participant.length(); i++) {
+            if (participant.charAt(i) == ',') {
+                name.add(participant.substring(1, i));
+                break;
+            }
+        }
+        for (int i = 0; i < participant.length(); i++) {
+            if ((participant.charAt(i) == '[' || participant.charAt(i) == ' ') && countFirst == 0) {
+                countFirst = i;
+            } else if ((participant.charAt(i) == ',' || participant.charAt(i) == ']') && countFirst != 0) {
+                name.add(participant.substring(countFirst + 1, i));
+                countFirst = 0;
+            }
+        }
+        return name;
+    }
+
+    private static ArrayList<String> getCompletion(String completion) {
+        int countFirst = 0;
+        ArrayList<String> name = new ArrayList<String>();
+
+        for (int i = 0; i < completion.length(); i++) {
+            if (completion.charAt(i) == ',') {
+                name.add(completion.substring(1, i));
+                break;
+            }
+        }
+        for (int i = 0; i < completion.length(); i++) {
+            if ((completion.charAt(i) == '[' || completion.charAt(i) == ' ') && countFirst == 0) {
+                countFirst = i;
+            } else if ((completion.charAt(i) == ',' || completion.charAt(i) == ']') && countFirst != 0) {
+                name.add(completion.substring(countFirst + 1, i));
+                countFirst = 0;
+            }
+        }
+        return name;
+    }
+
+    private static String solution(String participant, String completion) {
+        String answer = "";
         ArrayList<String> setParticipant = getParticipant(participant);
         ArrayList<String> setCompletion = getCompletion(completion);
 
-        for(int i=0;i<setParticipant.size();i++){
-            for(int j=0;j<setCompletion.size();j++){
-                if(setParticipant.get(i).equals(setCompletion.get(j))){
-                    setParticipant.remove(i);
-                    setCompletion.remove(j);
-                    i--;
-                    break;
-                }
-            }
-        }
-        System.out.println(setParticipant.get(0));
-    }
-    private static ArrayList<String> getParticipant(String participant){
-        int countFirst=0;
-        ArrayList<String> name = new ArrayList<String>();
+        setParticipant.sort(null);
+        setCompletion.sort(null);
 
-        for(int i=0;i<participant.length();i++){
-            if(participant.charAt(i)==','){
-                name.add(participant.substring(1,i));
+        for (int i = 0; i < setCompletion.size(); i++) {
+            if (!setParticipant.get(i).equals(setCompletion.get(i))) {
+                answer = setParticipant.get(i);
                 break;
             }
         }
-        for(int i=0;i<participant.length();i++){
-            if((participant.charAt(i) == '[' || participant.charAt(i)==' ') && countFirst==0){
-                countFirst=i;
-            }else if((participant.charAt(i)==',' || participant.charAt(i)==']') && countFirst!=0){
-                name.add(participant.substring(countFirst+1,i));
-                countFirst=0;
-            }
+        if (answer.equals("")) {
+            answer = setParticipant.get(setParticipant.size() - 1);
         }
-        return name;
-    }
-    private static ArrayList<String> getCompletion(String completion){
-        int countFirst=0;
-        ArrayList<String> name = new ArrayList<String>();
-
-        for(int i=0;i<completion.length();i++){
-            if(completion.charAt(i)==','){
-                name.add(completion.substring(1,i));
-                break;
-            }
-        }
-        for(int i=0;i<completion.length();i++){
-            if((completion.charAt(i) == '[' || completion.charAt(i)==' ') && countFirst==0){
-                countFirst=i;
-            }else if((completion.charAt(i)==',' || completion.charAt(i)==']') && countFirst!=0){
-                name.add(completion.substring(countFirst+1,i));
-                countFirst=0;
-            }
-        }
-        return name;
+        return answer;
     }
 }
